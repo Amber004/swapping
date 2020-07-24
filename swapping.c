d5235aba9ac7e51ab9dd81c4dff074e8e0ff49e4
Amber004 <68740593+Amber004@users.noreply.github.com>
2020-07-24 18:54:05 +0530

    Crated swapping.c
    this file swappes two number

---
+++ swapping.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int a = 4;
+int b = 6;
+int c;
+printf("Bfore swapping\n................\n");
+printf("a = %d\n",a);
+printf("b = %d\n",b);
+c = a;
+a = b;
+b = c;
+printf("After swapping\n................\n);
+printf("a = %d\n",a);
+printf("b = %d\n",b);
+return 0;
+}
+
